bb75a511cb001fabe324252db510386874823f6c
Sergio Marin <higher.vnf@gmail.com>
2012-07-31 19:43:49 -0430

    update grid on tag.php

--- tag.php
+++ tag.php
@@ -1,30 +1,16 @@
-<?php
-/**
- * The template for displaying Tag Archive pages.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<div class="row">
+	<div id="main" class="grid8">
+		<h1 class="page-title">
+			<?php printf( __( 'Tag Archives: %s', 'twentyten' ), '<span>' . single_tag_title( '', false ) . '</span>' );?>
+		</h1>
+		<?php get_template_part( 'loop', 'tag' ); ?>
+	</div><!-- #main -->
+	<div id="sidebar" class="grid4">
+		<?php get_sidebar(); ?>
+	</div>
+</div><!-- .row -->
 
-		<div id="container">
-			<div id="content" role="main">
 
-				<h1 class="page-title"><?php
-					printf( __( 'Tag Archives: %s', 'twentyten' ), '<span>' . single_tag_title( '', false ) . '</span>' );
-				?></h1>
-
-<?php
-/* Run the loop for the tag archive to output the posts
- * If you want to overload this in a child theme then include a file
- * called loop-tag.php and that will be used instead.
- */
- get_template_part( 'loop', 'tag' );
-?>
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
