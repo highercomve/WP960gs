b0377cbfb21b9b13d3e13cef1ba84afbb9e0e788
Sergio Marin <higher.vnf@gmail.com>
2013-04-05 20:18:19 -0430

    documentando la creacion de taxonomias

--- creacion_taxonomy.php
+++ creacion_taxonomy.php
@@ -8,17 +8,37 @@ en el function.php
 function customsCats_init() {
   // create a new taxonomy
   register_taxonomy(
-    'autor',
+    'pais', // Nombre que usara el sistema
     'post',
     array(
       'hierarchical' => true,
       'show_ui' => true,
       'query_var' => true,
-      'label' => __( 'Autores' ),
-      'rewrite' => array( 'slug' => 'autor' ),
+      'label' => __( 'Paises' ), // Nombre que va a ver el usuario
+      'rewrite' => array( 'slug' => 'pais' ), //Nombre que tendra en la ruta
     )
   );
 }
 add_action( 'init', 'customsCats_init' );
+
+?>
+
+<?php 
+/* 
+  Documentacion de como consultar las Taxonomias
+
+  $args = array (
+    "post_per_page" => "10", // cualquier filtro previo
+    "tax_query" => array (
+      array(
+        "taxonomy" => "pais",
+        "field" => "slug",
+        "terms" => "venezuela"
+      )
+    )
+  );
+
+  get_post($args);
+*/
 
  ?>
